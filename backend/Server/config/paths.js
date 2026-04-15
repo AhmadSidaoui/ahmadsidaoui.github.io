fd8103a1b2aad0bdc3e5552ba348bd610961503e
Ahmad Sidaoui <79225731+AhmadSidaoui@users.noreply.github.com>
2026-04-15 19:24:19 +0200

    updates

--- backend/Server/config/paths.js
+++ backend/Server/config/paths.js
@@ -7,8 +7,8 @@ const __dirname = path.dirname(__filename);
 export const ROOT_DIR = __dirname;
 
 export const CSV_FILES = {
-  data: path.join(__dirname, '../DocumentTraker.csv'),
-  chart: path.join(__dirname, '../savings_data.csv'),
-  bar: path.join(__dirname, '../cost_data.csv'),
-  task: path.join(__dirname, '../TaskManager.csv')
+  data: path.join(__dirname, '../../DocumentTraker.csv'),
+  chart: path.join(__dirname, '../../savings_data.csv'),
+  bar: path.join(__dirname, '../../cost_data.csv'),
+  task: path.join(__dirname, '../../TaskManager.csv')
 };
